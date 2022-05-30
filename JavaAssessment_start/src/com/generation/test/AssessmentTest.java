96b2e3fc1bf552192aa4bc24bafda4dc800f3c7a
Potato <102950453+Another-Potato@users.noreply.github.com>
2022-05-30 10:15:05 +0800

    Adding Gen given proj files

---
+++ JavaAssessment_start/src/com/generation/test/AssessmentTest.java
@@ -0,0 +1,23 @@
+package com.generation.test;
+
+/*
+import com.generation.java.Main;
+import org.junit.Before;
+import org.junit.Test;
+*/
+
+public class AssessmentTest {
+/*
+    private Main programInstance;
+
+    @Before
+    public void setup() {
+        programInstance = new Main();
+    }
+
+    @Test
+    public void test1() {
+
+    }*/
+
+}
